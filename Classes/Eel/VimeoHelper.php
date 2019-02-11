eba69f7ae78b8b58b0ea327ee89d218da8abe2e8
Jon Uhlmann <me@jonnitto.ch>
2019-02-11 23:18:46 +0100

    :bug: Parse also the title if the id is a url

--- Classes/Eel/VimeoHelper.php
+++ Classes/Eel/VimeoHelper.php
@@ -12,11 +12,44 @@ use Neos\Eel\ProtectedContextAwareInterface;
 class VimeoHelper implements ProtectedContextAwareInterface
 {
 
+    /**
+     * Get Vimeo video id from url
+     *
+     * Supported url formats -
+     *
+     * https://vimeo.com/11111111
+     * http://vimeo.com/11111111
+     * https://www.vimeo.com/11111111
+     * http://www.vimeo.com/11111111
+     * https://vimeo.com/channels/11111111
+     * http://vimeo.com/channels/11111111
+     * https://vimeo.com/groups/name/videos/11111111
+     * http://vimeo.com/groups/name/videos/11111111
+     * https://vimeo.com/album/2222222/video/11111111
+     * http://vimeo.com/album/2222222/video/11111111
+     * https://vimeo.com/11111111?param=test
+     * http://vimeo.com/11111111?param=test
+     *
+     * @param string $url The URL
+     *
+     * @return string the video id extracted from url
+     */
+
+    function parseID($url) {
+        if (!$url) {
+            return false;
+        }
+        $regs = array();
+        if (preg_match('%^https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|video\/|)(\d+)(?:$|\/|\?)(?:[?]?.*)$%im', $url, $regs)) {
+            return $regs[3];
+        }
+        return $url;
+    }
 
     /**
      * Grab the url of a image publicly embeddable video hosted on vimeo
      * @param string $video_id The "id" of a video
-     * @return string The url of the thumbnail, or false if there's an error
+     * @return string The data or false if there's an error
      */
     function data($id)
     {
@@ -27,12 +60,23 @@ class VimeoHelper implements ProtectedContextAwareInterface
         if (!$data) {
             return false;
         }
-        return [
-            'title' => $data->title,
-            'description' => $data->description,
-            'duration' => $data->duration,
-            'thumbnail' => $data->thumbnail_url
-        ];
+        return $data;
+    }
+
+    function title($id) {
+        $data = $this->data($id);
+        if (!$data) {
+            return false;
+        }
+        return $data->title;
+    }
+
+    function thumbnail($id) {
+        $data = $this->data($id);
+        if (!$data) {
+            return false;
+        }
+        return $data->thumbnail_url;
     }
 
     /**
