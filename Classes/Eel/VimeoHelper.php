1139d8eccaab312da4e15384bdfb79a11a74e226
Jon Uhlmann <me@jonnitto.ch>
2019-02-11 21:26:12 +0100

    :bug: Handle private videos

--- Classes/Eel/VimeoHelper.php
+++ Classes/Eel/VimeoHelper.php
@@ -20,9 +20,19 @@ class VimeoHelper implements ProtectedContextAwareInterface
      */
     function thumbnail($id)
     {
-        if (!$id) return false;
-        $data = json_decode(file_get_contents('http://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/' . $id . '&width=2560'));
-        if (!$data) return false;
+        if (!$id) {
+            return false;
+        }
+        $url = 'http://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/' . $id . '&width=2560';
+
+        if (substr(get_headers($url)[0], 9, 3) != '200') {
+            return false;
+        }
+
+        $data = json_decode(file_get_contents($url));
+        if (!$data) {
+            return false;
+        }
         return $data->thumbnail_url;
     }
 
